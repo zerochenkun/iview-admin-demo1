347510781951c9da5c93c5d382cac253a7d629cb
chenuvi <chenuvi@outlook.com>
2020-12-27 22:31:39 +0800

    demo: Render 、 Table on-expand

---
+++ src/pages/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    <!--    <h2>Table on-expand</h2>-->
+    <!--    <Table></Table>-->
+    <h2>Render demo</h2>
+    <Render></Render>
+
+  </div>
+</template>
+
+<script>
+  import Table from "./Table"
+  import Render from './Render.vue'
+
+  export default {
+    components: {Table, Render},
+  }
+</script>
